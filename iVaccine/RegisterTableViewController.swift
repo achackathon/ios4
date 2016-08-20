3a5216dce801ea8658ca77338909333dea4255f3
Giovanne Dias <giovanneed@gmail.com>
2016-08-20 11:01:00 -0300

    Create first view form register

---
+++ iVaccine/RegisterTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewController.swift
+//  iVaccine
+//
+//  Created by Ids Tecnologia  on 8/20/16.
+//  Copyright © 2016 Gio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterTableViewController : UITableViewController {
+    
+     
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
+
+//extension RegisterTableViewController : UITableViewDelegate {
+//    
+//}
+//
+//extension RegisterTableViewController : UITableViewDataSource {
+//    
+//}
